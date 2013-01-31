6854ae8186f760a4e54aa744c04c4859e57c41bf
Sam Bolgert <sam@livelovely.com>
2013-01-30 22:04:32 -0800

    working...maybe

--- app.py
+++ app.py
@@ -1,13 +1,46 @@
 from flask import Flask, request
+import re
+from twitter import *
+from tweetslice.app import _sendHtmlEmail
+#def _sendHtmlEmail(fromAddress, toAddress, title, textMessage, htmlMessage, fromName=None, toName=None, attachments=None, ignoreOverride=False, sender=None):
+
+consumer_key = 'Wx2lGU5aLowSXDxFNmMBzg'
+consumer_secret = 'pF9BYbosXaRZQaIORc4KNviY29LbtOBdEyfJmaAXnwA'
+
+oauth_token = '1135522998-RMEw1Z7MSmv3yWkFM3TszQX0MzWdkGOpmykVw1e'
+oauth_secret = 'ZxZ1ey5qyoUmpV5etMXfm3CktukaM2mVdCAkxIGgo'
 
 app = Flask(__name__)
 
+users = []
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	print 'yayyyyy'
-	print request.form
-	#pull user
-	return '1'
+	try:
+		to = request.form['to']
+		tweet_match = re.search('([\w]*)\+([\d]*)@maintenance.livelovely.com', to)
+		reply_user = tweet_match.group(1)
+		in_reply_to = tweet_match.group(2)
+
+		#forward email
+		from_ad = request.form['envelope']['from']
+		text = request.form['text']
+
+		user_email = users[reply_user]
+
+		_sendHtmlEmail(from_ad, user_email, 'RE:Lovely Maintenance Request', text, None)
+
+		#create twitter
+		t = Twitter(
+			auth=OAuth(oauth_token, oauth_secret,
+				consumer_key, consumer_secret)
+		)
+		tweet = '%s Your landlord has responded to your request. Check your email.' % '@'+reply_user
+		t.statuses.update(status=tweet, in_reply_to_status_id=in_reply_to)
+	except Exception as ex:
+		print ex.message
+	finally:
+		return '1'
 
 app.run(host='0.0.0.0', port=80)
 
